5916ba62314493371e9c0eb365d81e5e13ceee4f
feder1208 <fedesanchezdt@hotmail.com>
2020-09-13 20:11:43 -0300

    (version 1.0.0.0 Delilah)

---
+++ database/config/sequelize.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const host = "localhost";
+const name = "delilahresto";
+const port = "3306";
+const username = "root";
+const password = "";
+
+const path = `mysql://${username}:${password}@${host}:${port}/` + name;
+
+const sequelize = new Sequelize(path);
+const dependencies = { Sequelize: Sequelize,
+    sequelize: sequelize };
+
+
+module.exports = dependencies;
